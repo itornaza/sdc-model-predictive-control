92f745451f3df3febd4620552b19ecf6a81865ba
Ioannis Tornazakis <itornaza@gmail.com>
2017-10-30 11:29:48 +0200

    refactor: Style for MPC.cpp

--- src/MPC_orig.cpp
+++ src/MPC_orig.cpp
@@ -4,48 +4,42 @@
 #include "Eigen-3.3/Eigen/Core"
 
 using CppAD::AD;
-using namespace std;
 
 // TODO: Set the timestep length and duration
 size_t N = 0;
 double dt = 0;
 
-// This value assumes the model presented in the classroom is used. It was
-// obtained by measuring the radius formed by running the vehicle in the
+// This value assumes the model presented in the classroom is used.
+//
+// It was obtained by measuring the radius formed by running the vehicle in the
 // simulator around in a circle with a constant steering angle and velocity on a
-// flat terrain. Lf was tuned until the the radius formed by the simulating the
-// model presented in the classroom matched the previous radius.
+// flat terrain.
+//
+// Lf was tuned until the the radius formed by the simulating the model
+// presented in the classroom matched the previous radius.
+//
 // This is the length from front to CoG that has a similar radius.
 const double Lf = 2.67;
 
-/**
- * FG_eval class
- */
 class FG_eval {
  public:
   // Fitted polynomial coefficients
   Eigen::VectorXd coeffs;
   FG_eval(Eigen::VectorXd coeffs) { this->coeffs = coeffs; }
-  
+
   typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
-  
   void operator()(ADvector& fg, const ADvector& vars) {
     // TODO: implement MPC
-    // `fg` a vector of the cost constraints, `vars` is a vector of variable
-    // values (state & actuators)
+    // `fg` a vector of the cost constraints, `vars` is a vector of variable values (state & actuators)
     // NOTE: You'll probably go back and forth between this function and
     // the Solver function below.
   }
 };
 
-/**
- * MPC class implementation
- */
-
-// Constructor
+//
+// MPC class definition implementation.
+//
 MPC::MPC() {}
-
-// Destructor
 MPC::~MPC() {}
 
 vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
@@ -59,7 +53,6 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   //
   // 4 * 10 + 2 * 9
   size_t n_vars = 0;
-  
   // TODO: Set the number of constraints
   size_t n_constraints = 0;
 
@@ -72,7 +65,6 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
 
   Dvector vars_lowerbound(n_vars);
   Dvector vars_upperbound(n_vars);
-  
   // TODO: Set lower and upper limits for variables.
 
   // Lower and upper limits for the constraints
@@ -84,17 +76,16 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
     constraints_upperbound[i] = 0;
   }
 
-  // Object that computes objective and constraints
+  // object that computes objective and constraints
   FG_eval fg_eval(coeffs);
 
-  // TODO: Remove note after implementation
+  //
   // NOTE: You don't have to worry about these options
+  //
   // options for IPOPT solver
-  string options;
-  
+  std::string options;
   // Uncomment this if you'd like more print information
   options += "Integer print_level  0\n";
-  
   // NOTE: Setting sparse to true allows the solver to take advantage
   // of sparse routines, this makes the computation MUCH FASTER. If you
   // can uncomment 1 of these and see if it makes a difference or not but
@@ -102,15 +93,14 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   // magnitude.
   options += "Sparse  true        forward\n";
   options += "Sparse  true        reverse\n";
-  
   // NOTE: Currently the solver has a maximum time limit of 0.5 seconds.
   // Change this as you see fit.
   options += "Numeric max_cpu_time          0.5\n";
 
-  // Place to return solution
+  // place to return solution
   CppAD::ipopt::solve_result<Dvector> solution;
 
-  // Solve the problem
+  // solve the problem
   CppAD::ipopt::solve<Dvector, FG_eval>(
       options, vars, vars_lowerbound, vars_upperbound, constraints_lowerbound,
       constraints_upperbound, fg_eval, solution);
@@ -120,10 +110,12 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
 
   // Cost
   auto cost = solution.obj_value;
-  cout << "Cost " << cost << endl;
+  std::cout << "Cost " << cost << std::endl;
 
   // TODO: Return the first actuator values. The variables can be accessed with
-  // `solution.x[i]`. {...} is shorthand for creating a vector, so auto
-  // x1 = {1.0,2.0} creates a 2 element double vector.
+  // `solution.x[i]`.
+  //
+  // {...} is shorthand for creating a vector, so auto x1 = {1.0,2.0}
+  // creates a 2 element double vector.
   return {};
 }
