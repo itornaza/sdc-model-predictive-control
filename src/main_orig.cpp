3f2464dc448e3a2a83f4eca6da173d888ece72f0
Ioannis Tornazakis <itornaza@gmail.com>
2017-10-30 11:14:39 +0200

    refactor: Apply style for main.cpp

--- src/main_orig.cpp
+++ src/main_orig.cpp
@@ -9,9 +9,7 @@
 #include "MPC.h"
 #include "json.hpp"
 
-using namespace std;
-
-// For convenience
+// for convenience
 using json = nlohmann::json;
 
 // For converting back and forth between radians and degrees.
@@ -19,11 +17,9 @@ constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
-/**
- * Checks if the SocketIO event has JSON data.
- * If there is data the JSON object in string format will be returned,
- * else the empty string "" will be returned
- */
+// Checks if the SocketIO event has JSON data.
+// If there is data the JSON object in string format will be returned,
+// else the empty string "" will be returned.
 string hasData(string s) {
   auto found_null = s.find("null");
   auto b1 = s.find_first_of("[");
@@ -36,9 +32,7 @@ string hasData(string s) {
   return "";
 }
 
-/**
- * Evaluate a polynomial
- */
+// Evaluate a polynomial.
 double polyeval(Eigen::VectorXd coeffs, double x) {
   double result = 0.0;
   for (int i = 0; i < coeffs.size(); i++) {
@@ -47,10 +41,9 @@ double polyeval(Eigen::VectorXd coeffs, double x) {
   return result;
 }
 
-/**
- * Fit a polynomial, adapted from:
- * https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
- */
+// Fit a polynomial.
+// Adapted from
+// https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
 Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
                         int order) {
   assert(xvals.size() == yvals.size());
@@ -80,7 +73,7 @@ int main() {
 
   h.onMessage([&mpc](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
-    // "42" at the start of the message means there's a websocket message event
+    // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
     string sdata = string(data).substr(0, length);
@@ -100,99 +93,92 @@ int main() {
           double v = j[1]["speed"];
 
           /*
-           * TODO: Calculate steering angle and throttle using MPC.
-           *
-           * Both are in between [-1, 1].
-           *
-           */
-          
+          * TODO: Calculate steering angle and throttle using MPC.
+          *
+          * Both are in between [-1, 1].
+          *
+          */
           double steer_value;
           double throttle_value;
 
-          // Prepare the json message
           json msgJson;
-          
-          // TODO: Divide by deg2rad(25) before sending the steering value back.
-          // Otherwise the values will be in between [-deg2rad(25), deg2rad(25)]
-          // instead of [-1, 1]
+          // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
+          // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
 
-          // Display the MPC predicted trajectory
+          //Display the MPC predicted trajectory 
           vector<double> mpc_x_vals;
           vector<double> mpc_y_vals;
 
-          // TODO: add (x,y) points to list here, points are in reference to the
-          // vehicle's coordinate system the points in the simulator are
-          // connected by a Green line
+          //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
+          // the points in the simulator are connected by a Green line
+
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
 
-          // Display the waypoints/reference line
+          //Display the waypoints/reference line
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
-          // TODO: add (x,y) points to list here, points are in reference to the
-          // vehicle's coordinate system the points in the simulator are
-          // connected by a Yellow line
+          //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
+          // the points in the simulator are connected by a Yellow line
+
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
+
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          cout << msg << endl;
-          
-          //********************************************************************
-          // LATENCY
-          //********************************************************************
+          std::cout << msg << std::endl;
+          // Latency
           // The purpose is to mimic real driving conditions where
           // the car does actuate the commands instantly.
           //
-          // TODO: Remove Notes below after implementation:
-          // -  Feel free to play around with this value but should be to drive
-          //    around the track with 100ms latency.
-          // -  REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE
-          //    SUBMITTING.
+          // Feel free to play around with this value but should be to drive
+          // around the track with 100ms latency.
+          //
+          // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE
+          // SUBMITTING.
           this_thread::sleep_for(chrono::milliseconds(100));
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-        } // End - Telemetry
+        }
       } else {
         // Manual driving
-        string msg = "42[\"manual\",{}]";
+        std::string msg = "42[\"manual\",{}]";
         ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-      } // End if/else - Auto or manual driving
-    } // End if - Data checks
+      }
+    }
   });
 
-  /**
-   * We don't need this since we're not using HTTP but if it's removed the
-   * program doesn't compile :-(
-   */
+  // We don't need this since we're not using HTTP but if it's removed the
+  // program
+  // doesn't compile :-(
   h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data,
                      size_t, size_t) {
-    const string s = "<h1>Hello world!</h1>";
+    const std::string s = "<h1>Hello world!</h1>";
     if (req.getUrl().valueLength == 1) {
       res->end(s.data(), s.length());
     } else {
-      // I guess this should be done more gracefully?
+      // i guess this should be done more gracefully?
       res->end(nullptr, 0);
     }
   });
 
   h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-    cout << "Connected!!!" << endl;
+    std::cout << "Connected!!!" << std::endl;
   });
 
   h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
                          char *message, size_t length) {
     ws.close();
-    cout << "Disconnected" << endl;
+    std::cout << "Disconnected" << std::endl;
   });
 
   int port = 4567;
   if (h.listen(port)) {
-    cout << "Listening to port " << port << endl;
+    std::cout << "Listening to port " << port << std::endl;
   } else {
-    cerr << "Failed to listen to port" << endl;
+    std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
   h.run();
